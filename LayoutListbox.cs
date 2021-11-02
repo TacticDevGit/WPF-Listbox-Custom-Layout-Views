794d039cb1e25e5f6975540170586957963b9ce8
Tactic Dev <38791845+TacticDevGit@users.noreply.github.com>
2021-11-02 17:43:27 +0200

    version 1.0

---
+++ LayoutListbox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Listbox_Layouts
+{
+    public  class LayoutListbox : ListBox
+    {
+
+
+        public Layout layout
+        {
+            get { return (Layout)GetValue(layoutProperty); }
+            set { SetValue(layoutProperty, value); }
+        }
+
+        public static readonly DependencyProperty layoutProperty = DependencyProperty.Register("layout", typeof(Layout), typeof(LayoutListbox), new PropertyMetadata(Layout.List));
+
+
+    }
+
+
+
+
+    public enum Layout
+    {
+        Tile,
+        List
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Listbox_Layouts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var data = new[] { 
+            new { Name = "James" },
+            new { Name = "Mike" },
+            new { Name = "John" },
+            new { Name = "Luke" },
+            new { Name = "Oscar" },
+            new { Name = "Charles" },
+            };
+
+
+
+            MyListbox.ItemsSource = data;
+        }
+
+
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MyListbox.layout = Layout.Tile;
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            MyListbox.layout = Layout.List;
+        }
+
+
+    }
+}
